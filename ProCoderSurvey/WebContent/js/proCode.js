b13b00a5ece790bccdfe6c3f2afb01606a04a81b
RajeshnarayVer <rajesh.narayanan@verizon.com>
2015-08-28 08:45:00 -0700

    Update proCode.js

--- ProCoderSurvey/WebContent/js/proCode.js
+++ ProCoderSurvey/WebContent/js/proCode.js
@@ -2,7 +2,7 @@
  * 
  */
 
-
+var locations = [];
 var surveyData = '';
 var title='';
 var json={}; 
@@ -32,28 +32,33 @@ $(function() {
 $(document).ready(function(){
 	$('.content-secondary > div').css('display','none');
 	$('.content-secondary #survey-his-page').css('display','block');
-	$('#survey-list').css('display','block');
-	$('#survey-detail').css('display','none');
 	
 	getSurveyDetails();
 	
-	$(document.body).on('click','.surveyLink',function(){
-		$('#survey-list').css('display','none');
-		$('#survey-detail').css('display','block');
-		var surveyId = $(this).attr('id');
-		var surveyDetail = $(surveyData).find('survey').find('surveyId').filter(function(){return $.trim($(this).text()) == surveyId;}).parent('survey');
-		console.log(surveyDetail);
-		$('.cust-name').text($(surveyDetail).find('name').text());
-		$('.cust-add').text($(surveyDetail).find('address').text());
-		$('.room').text($(surveyDetail).find('room').text());
-		$('.type').text($(surveyDetail).find('type').text());
-		$('.by').text($(surveyDetail).find('by').text());
-		$('.date').text($(surveyDetail).find('data').text());
-		$('.rem').text($(surveyDetail).find('remarks').text());
-		$('.fileList').text($(surveyDetail).find('files').text());
-	});
 	
 });
+
+function loadSurveyDetail(obj){
+	$('.content-secondary > div').css('display','none');
+	$('.content-secondary #survey-his-detail-page').css('display','block');
+	var surveyId = $(obj).find('.issueId').val();
+	
+	var surveyDetail = $(surveyData).find('survey').find('surveyId').eq(surveyId).parent('survey');//.filter(function(){return $.trim($(this).text()) == surveyId;}).parent('survey');
+	
+	$('.cust-name1').text($(surveyDetail).find('name').text());
+	$('.cust-add1').text($(surveyDetail).find('address').text());
+	$('.room1').text($(surveyDetail).find('room').text());
+	$('.type1').text($(surveyDetail).find('type').text());
+	$('.by1').text($(surveyDetail).find('by').text());
+	$('.date1').text($(surveyDetail).find('date').text());
+	$('.rem1').text($(surveyDetail).find('remarks').text());
+	$('.fileList1').text($(surveyDetail).find('files').text());
+}
+
+
+function openMap(){
+	window.open('../jsp/NewFile.html','Location',width=100,height=150);
+}
 
 function skiplogin()
 {
@@ -89,13 +94,19 @@ function getSurveyDetails(){
 		success : function(result){
 			surveyData = result;
 			if($(result).find('survey').length == 0){
-				$('#survey-list').css('display','block');
-				$('#survey-detail').css('display','none');
 				$('#survey-list').html('<h4>Records not found. Please create a new survey</h4>');
 			}
 			$(result).find('survey').each(function(i){
 				var surveyId = $(this).find('surveyId').text();
-				$('#survey-list').append("<a class='surveyLink' id='"+surveyId+"'>Survey"+" "+i+"</a><br><br><br>")
+				//$('#survey-list').append("<a class='surveyLink' id='"+surveyId+"'>Survey"+" "+i+"</a><br><br><br>")
+				surveyTpl = [{
+					date:(($(this).find('date').text() == "" || $(this).find('date').text() == null) ? '&nbsp':$(this).find('date').text()),
+					name:(($(this).find('name').text() == "" || $(this).find('name').text() == null) ? '&nbsp':$(this).find('name').text()),
+					type:(($(this).find('type').text() == "" || $(this).find('type').text() == null) ? '&nbsp':$(this).find('type').text()),
+					by:(($(this).find('by').text() == "" || $(this).find('by').text() == null) ? '&nbsp':$(this).find('by').text()),
+					issueId:(($(this).find('surveyId').text() == "" || $(this).find('surveyId').text() == null) ? '&nbsp':$(this).find('surveyId').text())
+				}];
+				$('#SurveyDashboard').tmpl(surveyTpl).appendTo('#survey-list');
 			});
 		},
 		error : function(msg) {
@@ -166,4 +177,63 @@ function submitSurvey(){
 			});*/
 		}
 	});
+}
+
+function getLocation(){
+	  var geocoder;
+
+	  if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+	} 
+	//Get the latitude and the longitude;
+	function successFunction(position) {
+	    var lat = position.coords.latitude;
+	    var lng = position.coords.longitude;
+	    codeLatLng(lat, lng)
+	}
+
+	function errorFunction(){
+	    alert("Geocoder failed");
+	}
+
+	  function initialize() {
+	    geocoder = new google.maps.Geocoder();
+
+
+
+	  }
+
+	  function codeLatLng(lat, lng) {
+
+	    var latlng = new google.maps.LatLng(lat, lng);
+	    geocoder.geocode({'latLng': latlng}, function(results, status) {
+	      if (status == google.maps.GeocoderStatus.OK) {
+	      console.log(results)
+	        if (results[1]) {
+	         //formatted address
+	         alert(results[0].formatted_address)
+	        //find country name
+	             for (var i=0; i<results[0].address_components.length; i++) {
+	            for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+	            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+	                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+	                    //this is the object you are looking for
+	                    city= results[0].address_components[i];
+	                    break;
+	                }
+	            }
+	        }
+	        //city data
+	        $('#address').text(city.short_name + " " + city.long_name);
+
+
+	        } else {
+	          alert("No results found");
+	        }
+	      } else {
+	        alert("Geocoder failed due to: " + status);
+	      }
+	    });
+	  }
 }
